3c31a8faebcc5c801d629b8ca3a54510e9ae9e9e
AlgoTechLand <algotechland@gmail.com>
2023-02-28 08:58:21 +0600

    up

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose"); // new
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.BASE_DB, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
 });
